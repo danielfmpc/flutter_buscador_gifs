948882f9b93acf33bb39a67f1f9de15de7052613
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-05-08 22:58:18 -0300

    terminando com a API

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,14 +10,18 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   String _search;
-  _getGifs() async {
+  int _offset;
+  Future<Map> _getGifs() async {
     http.Response response;
 
     if (_search == null)
       response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=nr4Dvft5yHSY2x83Y3hxY6kTO1o99t3T&limit=20&rating=G");
     else 
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=nr4Dvft5yHSY2x83Y3hxY6kTO1o99t3T&q=$_search&limit=25&offset=75&rating=G&lang=en");
+      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=nr4Dvft5yHSY2x83Y3hxY6kTO1o99t3T&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
+    
+    return jsonDecode(response.body);
   }
+  
   @override
   Widget build(BuildContext context) {
     return Container(
